1f1518f5d298b48c1c712603287540a80f415e79
ImJustLucas <bellierlucas.pro@gmail.com>
2023-06-14 22:42:25 +0200

    ✨ init shared types

---
+++ packages/shared-types/src/index.ts
@@ -0,0 +1,10 @@
+export * from "./user";
+
+/**
+ * @description
+ * This is the language enum.
+ */
+export enum Language {
+  FR,
+  EN,
+}
---
+++ packages/shared-types/src/user/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @description
+ * This is the user interface.
+ */
+export interface User {
+  id: string;
+  username: string;
+  password: string;
+}
